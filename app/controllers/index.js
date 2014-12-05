66a271be62097b8f0b0a7a85c68cecc69dfbef70
Arseny Zarechnev <cyanidesign@gmail.com>
2014-12-06 00:07:57 +0700

    move openWallet action to index controller

---
+++ app/controllers/index.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+/* global bitcoin */
+
+export default Ember.Controller.extend({
+    needs: ['application'],
+
+    actions: {
+        openWallet: function(seed) {
+            var wallet = new bitcoin.Wallet(bitcoin.crypto.sha256(seed));
+            this.get('controllers.application').set('wallet', wallet);
+            // I'm feeling guilty for doing this, but i couldn't find
+            // another solution to get props from controller in adapters
+            // @see http://stackoverflow.com/questions/19818957/accessing-controller-properties-within-ember-data-rest-adapter
+            window._address = wallet.getAddress();
+            this.transitionToRoute('wallet/dashboard');
+            return false;
+        }
+    }
+});
+
