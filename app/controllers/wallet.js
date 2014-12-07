c6e2ebc78ebcfa93352b9ae384abd4f878581e2e
Arseny Zarechnev <cyanidesign@gmail.com>
2014-12-08 02:00:25 +0700

    styling

--- app/controllers/wallet.js
+++ app/controllers/wallet.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
-  sortProperties: ['block_time'],
+  sortProperties: ['confirmations'],
 
   init: function() {
     var wallet = this.get('controllers.application').get('wallet');
@@ -28,29 +28,22 @@ export default Ember.Controller.extend({
         blockChain: "bitcoin",
         apiKeyId: "185fcf18e6208415c5f037895aeb484c"
       });
-      chain.transact(
-        {
-        inputs: [
-          {
+
+      chain.transact({
+        inputs: [{
           address: window._address,
           private_key: wif
-        }
-        ],
-        outputs: [
-          {
+        }],
+        outputs: [{
           address: data.address,
           amount: data.amount * 100000000
-        }
-        ]
+        }]
       }, function(err, resp) {
+        //error handling
         console.log('=================================')
         console.log(arguments);
         console.log('=================================')
       });
-    },
-
-    showQRCode: function() {
-      
     }
   }
 });
