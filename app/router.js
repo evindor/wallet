559219772b0785639a6a166d8aaf3e69dfd0493b
Arseny Zarechnev <cyanidesign@gmail.com>
2014-12-08 02:01:56 +0700

    remove simpleauth rudiment

--- app/router.js
+++ app/router.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import config from './config/environment';
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 var Router = Ember.Router.extend({
   location: config.locationType
@@ -12,4 +11,4 @@ Router.map(function() {
   this.route('logout');
 });
 
-export default Router.extend(ApplicationRouteMixin);
+export default Router;
